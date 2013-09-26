20bd9a6a8398ac30e78ef923cbe3e23d56f9d889
Jim Purbrick <jimp@fb.com>
2013-09-26 22:20:39 +0100

    fix syntax

--- MoodMusicCloud/cloud/main.js
+++ MoodMusicCloud/cloud/main.js
@@ -1,9 +1,8 @@
 Parse.Cloud.define("graphData", function(request, response) {
-    var oauthToken = request.params.oauth_token;
-    
+
     // Get listens from graph.
     Parse.Cloud.httpRequest({
-	url: 'https://graph.facebook.com/me/music.listens?fields=data%2Cstart_time&method=GET&format=json&suppress_http_code=1&access_token=' + oauthToken;
+	url: 'https://graph.facebook.com/me/music.listens?fields=data%2Cstart_time&method=GET&format=json&suppress_http_code=1&access_token=' + request.params.oauth_token,
 	success: function(httpResponse) {
 	    console.log(httpResponse.text);
 	},
