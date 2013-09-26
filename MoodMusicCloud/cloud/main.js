a23c48f7779b433798f10a988459dcbefe38c4f4
Jim Purbrick <jimp@fb.com>
2013-09-26 23:31:43 +0100

    return dummy listen data

--- MoodMusicCloud/cloud/main.js
+++ MoodMusicCloud/cloud/main.js
@@ -1,15 +1,22 @@
 Parse.Cloud.define("graphData", function(request, response) {
 
+    var url = 'https://graph.facebook.com/me/music.listens?fields=data%2Cstart_time&access_token=' + request.params.oauth_token;
+    console.log(url);
+
     // Get listens from graph.
     Parse.Cloud.httpRequest({
-	url: 'https://graph.facebook.com/me/music.listens?fields=data%2Cstart_time&method=GET&format=json&suppress_http_code=1&access_token=' + request.params.oauth_token,
+	url: url,
 	success: function(httpResponse) {
 	    console.log(httpResponse.text);
-	    response.success("w00t");
 	},
 	error: function(httpResponse) {
 	    console.error('Request failed with code ' + httpResponse.status);
-	    response.error("meh");
 	}
     });
+
+
+    response.success({
+	listen_data: [[0.1, 0.2, 0.3], [0.1, 0.2, 0.3], [0.1, 0.2, 0.3], [0.1, 0.2, 0.3], [0.1, 0.2, 0.3], [0.2, 0.4, 0.6], [0.1, 0.2, 0.3],],
+    });
+
 });
