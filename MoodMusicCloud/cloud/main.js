d75e83e9e493939c76c163ce312310420759c917
Jim Purbrick <jimp@fb.com>
2013-09-26 22:59:04 +0100

    cp

--- MoodMusicCloud/cloud/main.js
+++ MoodMusicCloud/cloud/main.js
@@ -5,9 +5,11 @@ Parse.Cloud.define("graphData", function(request, response) {
 	url: 'https://graph.facebook.com/me/music.listens?fields=data%2Cstart_time&method=GET&format=json&suppress_http_code=1&access_token=' + request.params.oauth_token,
 	success: function(httpResponse) {
 	    console.log(httpResponse.text);
+	    response.success("w00t");
 	},
 	error: function(httpResponse) {
 	    console.error('Request failed with code ' + httpResponse.status);
+	    response.error("meh");
 	}
     });
 });
