5abf07e15d083f34d9f7fa813d6c47b63af21150
Jim Purbrick <jimp@fb.com>
2013-09-26 22:17:20 +0100

    initial graph api call

--- MoodMusicCloud/cloud/main.js
+++ MoodMusicCloud/cloud/main.js
@@ -1,6 +1,14 @@
-
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.define("graphData", function(request, response) {
+    var oauthToken = request.params.oauth_token;
+    
+    // Get listens from graph.
+    Parse.Cloud.httpRequest({
+	url: 'https://graph.facebook.com/me/music.listens?fields=data%2Cstart_time&method=GET&format=json&suppress_http_code=1&access_token=' + oauthToken;
+	success: function(httpResponse) {
+	    console.log(httpResponse.text);
+	},
+	error: function(httpResponse) {
+	    console.error('Request failed with code ' + httpResponse.status);
+	}
+    });
 });
